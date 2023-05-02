7e7ade66fef4fe19a25cc2ecb443297ba6905b8a
katherine Buendia <kbbuendia24@gmail.com>
2023-05-02 19:23:40 -0400

    Linq Excersice update

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine();
+
+      
+
+            List<string> games = new List<string>() { "Call Of Duty" , "Roblox", " Rocket League", " Grand T Auto"};
+
+            var NumOfGames = games.OrderBy(name => name.Length);
+
+            foreach ( var game in NumOfGames)
+            {
+                Console.WriteLine( game);
+            }
         }
     }
 }
